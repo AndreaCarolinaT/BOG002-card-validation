2a0bd0a584b69eb600f24bcda85979d5ef36adf3
Andy164 <apenarandao@ufpso.edu.co>
2021-02-25 08:15:49 -0500

    Cambios para la segunda página y función de botón

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import validator from './validator.js';
 
+const sDButton = document.getElementById('seeDetailsButton');
+sDButton.addEventListener('click', function() {
+    location.href = 'detailsCourse.html';
+});
+
 console.log(validator);
