ccea2746570b68e771c1f788e9c112fa9010c05f
AndreaCarolinaT <andrea.2612@hotmail.com>
2021-02-26 11:10:21 -0500

    cambios en páginas de descripciones de cursos 1 y 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import validator from './validator.js';
 
-const sDButton = document.getElementById('seeDetailsButton');
-sDButton.addEventListener('click', function() {
-    location.href = 'detailsCourse.html';
-});
+const sDButton = document.getElementById("seeDetailsButton");
+sDButton.addEventListener("click", function () {
+ location.href = "detailsCourse.html";
+})
 
-console.log(validator);
+const sDButton2 = document.getElementById("seeDetailsButton2");
+sDButton.addEventListener("click", function () {
+ location.href = "detailsCourse2.html";
+})
+
+const sDButton3 = document.getElementById("seeDetailsButton3");
+sDButton.addEventListener("click", function () {
+ location.href = "detailsCourse3.html";
+})
+
+const sDButton4 = document.getElementById("seeDetailsButton4");
+sDButton.addEventListener("click", function () {
+ location.href = "detailsCourse4.html";
+})
